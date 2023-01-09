32803807052bd2085abebfa8df1e2c3ba673fabb
1oooton <mjmilton767@gmail.com>
2023-01-09 09:29:09 -0300

    terminado completo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -74,7 +74,7 @@ function sonIguales(x, y) {
         return true;
             }
              else return false;
-
+// return x===y? true:false 
   
 }
 
@@ -150,6 +150,7 @@ function elevarAlCuadrado(num) {
   // Tu código:
       var cuadrado= num*num;
       return cuadrado;
+     
   
 }
 
@@ -183,7 +184,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  return Math.random();
+  return Math.random(0,1);
   
 }
 
